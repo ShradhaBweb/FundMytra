9ec2bb0f76c233c0007757c11821477933de75e0
pavitroBweb <pavitro.bweb>
2018-09-05 18:36:37 +0530

    loginScreen

---
+++ app/src/main/java/com/example/bcs/fundmytra/Activity/otpscreen.java
@@ -0,0 +1,15 @@
+package com.example.bcs.fundmytra.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.bcs.fundmytra.R;
+
+public class otpscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otpscreen);
+    }
+}
--- app/src/main/java/com/example/bcs/fundmytra/IntroductionActivity.java
+++ app/src/main/java/com/example/bcs/fundmytra/IntroductionActivity.java
@@ -1,13 +1,35 @@
 package com.example.bcs.fundmytra;
 
+import android.graphics.Typeface;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.widget.TextView;
 
 public class IntroductionActivity extends AppCompatActivity {
 
+    TextView textView;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introduction);
+        getSupportActionBar().hide();
+        String text1="fund";
+        String text2="mytra";
+        int h=R.string.logtext_1;
+
+        textView=(TextView)findViewById(R.id.logText_one);
+
+        String text=text1+
+                "<b>"+text2+"</b>";
+        textView.setText(Html.fromHtml(text));
+
+
     }
 }
