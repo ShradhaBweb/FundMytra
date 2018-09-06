1077785a35bf8bc54f3e0897071fa0b2569fef2d
Manoj <manojkumar.bweb@gmail.com>
2018-09-06 14:49:58 +0530

    added Login and Signup Screens

--- app/src/main/java/com/example/bcs/fundmytra/Login_Signup.java
+++ app/src/main/java/com/example/bcs/fundmytra/Login_Signup.java
@@ -1,16 +1,27 @@
 package com.example.bcs.fundmytra;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Login_Signup extends AppCompatActivity {
-private TextView hello;
+    private Button signup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_signup);
+        setContentView(R.layout.activity_login__signup);
 
-
+        signup=(Button)findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Login_Signup.this,signup_screen.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
