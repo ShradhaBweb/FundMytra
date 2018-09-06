34593b48937689a553cefc4bad272d10abc61b39
pavitroBweb <pavitro.bweb>
2018-09-06 16:09:38 +0530

    login_page and otp_Page

--- app/src/main/java/com/example/bcs/fundmytra/IntroductionActivity.java
+++ app/src/main/java/com/example/bcs/fundmytra/IntroductionActivity.java
@@ -1,5 +1,6 @@
 package com.example.bcs.fundmytra;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
@@ -12,23 +13,13 @@ import android.widget.TextView;
 
 public class IntroductionActivity extends AppCompatActivity {
 
-    TextView textView;
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent=new Intent(this,Otp_activity.class);
+        startActivity(intent);
         setContentView(R.layout.activity_introduction);
-        getSupportActionBar().hide();
-        String text1="fund";
-        String text2="mytra";
-        int h=R.string.logtext_1;
 
-        textView=(TextView)findViewById(R.id.logText_one);
-
-        String text=text1+
-                "<b>"+text2+"</b>";
-        textView.setText(Html.fromHtml(text));
 
 
     }
