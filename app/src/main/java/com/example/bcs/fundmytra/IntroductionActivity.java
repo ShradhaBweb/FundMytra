3c167e1ad897c65e9f5d3a778fd9e43aed998e1e
Manoj <manojkumar.bweb@gmail.com>
2018-09-06 14:30:49 +0530

    intial commit

--- app/src/main/java/com/example/bcs/fundmytra/IntroductionActivity.java
+++ app/src/main/java/com/example/bcs/fundmytra/IntroductionActivity.java
@@ -9,7 +9,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,7 +17,6 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class IntroductionActivity extends AppCompatActivity {
 
@@ -134,7 +132,7 @@ public class IntroductionActivity extends AppCompatActivity {
 
     private void launchHomeScreen() {
         prefManager.setFirstTimeLaunch(false);
-        startActivity(new Intent(IntroductionActivity.this, HomeActivity.class));
+        startActivity(new Intent(IntroductionActivity.this, Login_Signup.class));
         finish();
     }
 
--- app/src/main/java/com/example/bcs/fundmytra/Login_Signup.java
+++ app/src/main/java/com/example/bcs/fundmytra/Login_Signup.java
@@ -2,12 +2,15 @@ package com.example.bcs.fundmytra;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
-
+public class Login_Signup extends AppCompatActivity {
+private TextView hello;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_login_signup);
+
+
     }
 }
