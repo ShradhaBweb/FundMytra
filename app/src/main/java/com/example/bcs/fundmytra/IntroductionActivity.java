2bc8d2a62275601ff88723e559324074b379e02e
pavitroBweb <pavitro.bweb>
2018-09-10 09:59:02 +0530

    changes in class name and also logIn and otp page code.delete previous login and otp page

--- app/src/main/java/com/example/bcs/fundmytra/IntroductionActivity.java
+++ app/src/main/java/com/example/bcs/fundmytra/IntroductionActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class IntroductionActivity extends AppCompatActivity {
@@ -16,11 +18,9 @@ public class IntroductionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent=new Intent(this,Otp_activity.class);
+        Intent intent=new Intent(this,LoginActivity.class);
         startActivity(intent);
         setContentView(R.layout.activity_introduction);
-
-
 
     }
 }
