c8527e690a949342ba8ee3040e1a0e3168873092
Manoj <manojkumar.bweb@gmail.com>
2018-09-19 12:20:43 +0530

    My Searches  added

---
+++ app/src/main/java/com/example/bcs/fundmytra/DataModel.java
@@ -0,0 +1,53 @@
+package com.example.bcs.fundmytra;
+
+public class DataModel {
+
+
+    private int prdouct;
+    private String name;
+    private String searched_on;
+    private String time_date;
+
+    public int getPrdouct() {
+        return prdouct;
+    }
+
+    public void setPrdouct(int prdouct) {
+        this.prdouct = prdouct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSearched_on() {
+        return searched_on;
+    }
+
+    public void setSearched_on(String searched_on) {
+        this.searched_on = searched_on;
+    }
+
+    public String getTime_date() {
+        return time_date;
+    }
+
+    public void setTime_date(String time_date) {
+        this.time_date = time_date;
+    }
+
+    // Constructor that is used to create an instance of the Movie object
+    public DataModel(int product, String name, String searched_on,String time_date) {
+
+        this.prdouct = product;
+        this.name = name;
+        this.searched_on = searched_on;
+        this.time_date=time_date;
+    }
+
+
+}
--- app/src/main/java/com/example/bcs/fundmytra/MySearchesFragment.java
+++ app/src/main/java/com/example/bcs/fundmytra/MySearchesFragment.java
@@ -8,11 +8,15 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
 
 public class MySearchesFragment extends Fragment {
-    ListView simpleList;
-    String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand"};
-
+   // ListView simpleList;
+   // String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand"};
+    private ListView listView;
+    private CustomAdapter mAdapter;
     public MySearchesFragment() {
 // Required empty public constructor
     }
@@ -21,16 +25,23 @@ public class MySearchesFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mysearches, container, false);
-    //    simpleList = (ListView)container.findViewById(R.id.simpleListView);
-       // ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, countryList);
-      //  simpleList.setAdapter(arrayAdapter);
+        RelativeLayout relativeLayout=(RelativeLayout) inflater.inflate(R.layout.fragment_mysearches, container, false);
+        listView = (ListView)relativeLayout.findViewById(R.id.simpleListView);
+        ArrayList<DataModel> moviesList = new ArrayList<>();
+        moviesList.add(new DataModel(R.drawable.car, "Car Loan" , "searched on","10-10-2018 at 03:45PM"));
+        moviesList.add(new DataModel(R.drawable.house, "House Loan" , "searched on","10-10-2018 at 03:45PM"));
+        moviesList.add(new DataModel(R.drawable.credit, "Credit Card" , "searched on","10-10-2018 at 03:45PM"));
+        moviesList.add(new DataModel(R.drawable.top_up, "Top up Car loan" , "searched on","10-10-2018 at 03:45PM"));
+        mAdapter = new CustomAdapter(getContext(),moviesList);
+        listView.setAdapter(mAdapter);
+        return relativeLayout;
     }
 
 }
