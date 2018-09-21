981a4132ef1abddba585e6c38f010db3f7075296
pavitroBweb <pavitro.bweb>
2018-09-21 11:38:14 +0530

    AllProductPageAdapter

---
+++ app/src/main/java/com/example/bcs/fundmytra/AllProductPageAdapter.java
@@ -0,0 +1,73 @@
+package com.example.bcs.fundmytra;
+
+import android.content.Context;
+import android.os.Parcelable;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.github.siyamed.shapeimageview.RoundedImageView;
+
+import java.util.ArrayList;
+
+public class AllProductPageAdapter  extends PagerAdapter {
+
+    private ArrayList<Integer> IMAGES;
+    private LayoutInflater inflater;
+    private Context context;
+
+    
+
+
+    public AllProductPageAdapter(Context context, ArrayList<Integer> IMAGES) {
+        this.context = context;
+        this.IMAGES = IMAGES;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public int getCount() {
+        return IMAGES.size();
+    }
+
+
+    @Override
+    public Object instantiateItem(ViewGroup view, int position) {
+        View imageLayout = inflater.inflate(R.layout.allproductpageadapter, view, false);
+
+        assert imageLayout != null;
+        final ImageView imageView = (ImageView) imageLayout
+                .findViewById(R.id.image_view);
+
+
+        imageView.setImageResource(IMAGES.get(position));
+
+        view.addView(imageLayout, 0);
+
+        return imageLayout;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view.equals(object);
+    }
+
+    @Override
+    public void restoreState(Parcelable state, ClassLoader loader) {
+    }
+
+    @Override
+    public Parcelable saveState() {
+        return null;
+    }
+
+
+}
