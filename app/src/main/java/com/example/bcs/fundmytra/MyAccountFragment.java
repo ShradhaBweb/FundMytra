11de9bfc6182667083b188968976573dc636e60d
ShradhaBweb <shradha.bweb@gmail.com>
2018-09-25 15:31:05 +0530

    Removed unwanted Activity

--- app/src/main/java/com/example/bcs/fundmytra/MyAccountFragment.java
+++ app/src/main/java/com/example/bcs/fundmytra/MyAccountFragment.java
@@ -2,7 +2,10 @@ package com.example.bcs.fundmytra;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabItem;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +16,9 @@ import android.view.ViewGroup;
  */
 public class MyAccountFragment extends Fragment {
 
-
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    FragmentAdapter adapter;
     public MyAccountFragment() {
         // Required empty public constructor
     }
@@ -23,7 +28,48 @@ public class MyAccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_my_account, container, false);
+
+        TabItem tabprofile = view.findViewById(R.id.my_profile);
+        TabItem tabsearches = view.findViewById(R.id.my_searches);
+        TabItem tabaplication = view.findViewById(R.id.my_application);
+        TabItem tabinterest = view.findViewById(R.id.salary_info);
+        TabItem tabsalaryinformation = view.findViewById(R.id.documents);
+        tabLayout = (TabLayout) view.findViewById(R.id.simpleTabLayout);
+        viewPager = (ViewPager)view. findViewById(R.id.viewpager);
+
+
+        init();
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+        listeners();
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        return view;
+    }
+    private void init() {
+
+        adapter = new FragmentAdapter(getActivity(), getChildFragmentManager(),tabLayout.getTabCount());
+
+    }
+
+    private void listeners() {
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 
 }
